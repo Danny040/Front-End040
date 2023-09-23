48a245d470fd715f6ec1bc722d943b7148e46c8b
Danny <71259569+Danny040@users.noreply.github.com>
2023-09-23 14:05:35 +0300

    Comments main.js
    comments are added for better understanding

--- forms/Form3/main.js
+++ forms/Form3/main.js
@@ -1,23 +1,23 @@
-const loginWindow = () => {
+const loginWindow = () => { // opens and closes login form
   const logIn = document.querySelector(".login");
   const sideMenu = document.getElementById("one");
   const closeX = document.querySelector(".close-x");
   const logInForm = document.querySelector(".log-in-hide");
-
+// addes event listener to log in button
   logIn.addEventListener("click", () => {
     if (sideMenu.classList.contains("side-nav")) {
       sideMenu.classList.add("hide");
       sideMenu.classList.remove("side-nav");
       logInForm.classList.toggle("log-in-display");
     } else {
-      logInForm.classList.toggle("log-in-display");
+      logInForm.classList.toggle("log-in-display");// adds or removes class to show or hide the login form
     }
   });
-  closeX.addEventListener("click", () => {
+  closeX.addEventListener("click", () => { // closeX - close button on the form
     logInForm.classList.remove("log-in-display");
   });
 };
-
+// shows and hides side bar on smaller screens
 const navBar = () => {
   const burgerMenu = document.querySelector(".hamburger-menu");
   const sideMenu = document.getElementById("one");
@@ -32,6 +32,6 @@ const navBar = () => {
     }
   });
 };
-
+// invoke arrow functions
 loginWindow();
 navBar();
