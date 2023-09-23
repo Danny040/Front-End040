fa29aba443a64a0b2ca47e50a1152bdc683b457e
Danny <71259569+Danny040@users.noreply.github.com>
2023-09-23 16:24:14 +0300

    Comments main.js
    comments are added for better understanding

--- parallax site/main.js
+++ parallax site/main.js
@@ -1,14 +1,14 @@
-let navBar = () => {
+let navBar = () => { // this function shows and hides side navigation menu
   const burgerMenu = document.querySelector("#burger");
   const closeBt = document.querySelector("#close");
   const menuList = document.querySelector(".list-menu-wrapper");
 
-  burgerMenu.addEventListener("click", () => {
+  burgerMenu.addEventListener("click", () => { // burger menu button
     menuList.classList.remove("list-menu-wrapper");
     menuList.classList.add("list-menu-wrapper-show");
   });
 
-  closeBt.addEventListener("click", () => {
+  closeBt.addEventListener("click", () => { // close button
     menuList.classList.remove("list-menu-wrapper-show");
     menuList.classList.add("list-menu-wrapper");
   });
